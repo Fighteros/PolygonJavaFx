6af0f55352a3056a96b63faa1ed83e468d390234
fighteros <fighteros121212@gmail.com>
2021-06-19 15:46:26 +0200

    committed

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,40 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+        Polygon polygon = new Polygon();
+        pane.getChildren().add(polygon);
+
+        polygon.setFill(Color.GOLD);
+        polygon.setStroke(Color.BLACK);
+
+        ObservableList<Double> obsPoly = polygon.getPoints();
+
+        final double WIDTH = 200, HEIGHT = 200;
+
+        double centerX = WIDTH / 2, centerY = HEIGHT / 2;
+        double radius = Math.min(WIDTH, HEIGHT) * .4;
+
+        for(int i = 0; i < 6; i++) {
+            obsPoly.add(centerX + radius * Math.cos(2* i * Math.PI /6)) ;
+            obsPoly.add(centerY - radius * Math.sin(2* i * Math.PI /6)) ;
+        }
+        primaryStage.setTitle("Polygon");
+        primaryStage.setScene(new Scene(pane, WIDTH, HEIGHT));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
